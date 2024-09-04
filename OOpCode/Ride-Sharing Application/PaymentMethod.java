a0057c1ad5409f991c0bc80b960156641495ab1e
RobiulHasaN <145006342+RoBiUlHaSaNj@users.noreply.github.com>
2024-09-04 08:05:16 +0600

    Abstract

---
+++ OOpCode/Ride-Sharing Application/PaymentMethod.java
@@ -0,0 +1,29 @@
+interface PaymentMethod {
+    void processPayment(double amount);
+}
+
+class CreditCardPayment implements PaymentMethod {
+    private String cardNumber;
+
+    public CreditCardPayment(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing credit card payment of $" + amount + " using card " + cardNumber);
+    }
+}
+
+class PayPalPayment implements PaymentMethod {
+    private String email;
+
+    public PayPalPayment(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("Processing PayPal payment of $" + amount + " using email " + email);
+    }
+}
